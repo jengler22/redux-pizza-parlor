22e939a4056155069cf8e03463389358708370d4
Glonel Dimapilis <gdimapilis@aol.com>
2023-04-13 19:05:40 -0500

    Added Router and Routes to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter  as Router, Route, Link } from 'react-router-dom';
+import Header from '../Header/Header.jsx';
+import PizzaList from '../PizzaList/PizzaList.jsx';
+import CustomerForm from '../CustomerForm/CustomerForm.jsx';
+import Checkout from'../Checkout/Checkout.jsx';
+import Admin from '../Admin/Admin.jsx';
+
 
 function App() {
 
   return (
     <div className='App'>
+      <Router>
+        <Header />
+        <Route exact path="/">
+          <PizzaList />
+        </Route>
+        <Route exact path="/customer-form">
+          <CustomerForm />
+        </Route>
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="/admin">
+          <Admin />
+        </Route>
+      </Router>
+
+
+
+      {/* extra code if you need it :D */}
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
